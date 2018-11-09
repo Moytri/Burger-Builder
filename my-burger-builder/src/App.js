5fb39340ade1a3b2733d4866110c795d54c005df
moytri <moytritopa@gmail.com>
2018-11-09 14:57:54 -0800

    add layout

--- my-burger-builder/src/App.js
+++ my-burger-builder/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
-
+import Layout from './components/Layout/Layout';
 class App extends Component {
   render() {
     return (
-      <div className={classes.App}>
-        <p className={classes.front}>Hello</p>
+      <div>
+        <Layout>
+            <p>Hello</p>
+        </Layout>
       </div>
     );
   }
