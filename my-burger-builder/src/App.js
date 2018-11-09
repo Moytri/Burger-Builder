51c6cc1a0dabb6abaa634f7ebc43dcbaf1252730
moytri <moytritopa@gmail.com>
2018-11-09 12:00:48 -0800

    add project env

---
+++ my-burger-builder/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './App.module.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <p className={classes.front}>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default App;
