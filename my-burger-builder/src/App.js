37fdf0f90142770898ab1a36e79660e97bbfd7d6
moytri <moytritopa@gmail.com>
2018-11-09 15:12:22 -0800

    add css class

--- my-burger-builder/src/App.js
+++ my-burger-builder/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout>
-            <p>Hello</p>
+        <Layout> 
+            <BurgerBuilder/>
         </Layout>
       </div>
     );
