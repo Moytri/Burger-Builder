1a1d380f25f6b10f97cae1f9558ff5ddd618d640
moytri <moytritopa@gmail.com>
2018-11-10 12:13:06 -0800

    include constructor and state

--- my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,13 +4,28 @@ import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
 
+    constructor (props) {
+        super(props);
+        this.state = {
+                //key value pair
+                //key is the name of the ingredients
+                //value is the amount
+                ingredients: {
+                    salad: 1,
+                    bacon: 1,
+                    cheese: 2,
+                    meat: 2
+                }
+        }
+    }
+
     render() {
         //returns two components
         //graphical representation of the burger
         //The area where I need to add or remove Ingredients
         return (
             <Aux>             
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
             </Aux>
         );
     };
