fbfc0fda213c123d674a0d4a9dfcdb6342674b58
moytri <moytritopa@gmail.com>
2018-11-10 12:12:38 -0800

    loop props Object

--- my-burger-builder/src/components/Burger/Burger.js
+++ my-burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,28 @@ import BurgerIngredient from './BurgerIgredient/BurgerIgredient';
 import classes from './Burger.module.css';
 
 const burger = (props) => {
+    //props.ingredients is an Object
+    //keys method extract the keys of the given Object
+    //returns a array of keys
+    //igKey is the key of the array
+
+    //**************************** */
+
+    //Array(props.ingredients[igKey] creates a new Array like new Array(3)
+    //means the size of array is 3
+    //here we creates array having size that equals the amount of given ingredient (igKey)
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map((igKey) => {
+            return [...Array(props.ingredients[igKey])] 
+                    .map((_, i) => {
+                        return <BurgerIngredient key={igKey + i} type={igKey} />;
+                    });
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
