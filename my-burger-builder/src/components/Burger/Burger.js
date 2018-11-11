6d1886a6940b0ee6f8b9cccc2c8429d4ecd87aa2
moytri <moytritopa@gmail.com>
2018-11-10 16:31:22 -0800

    add dynamic ingredients

--- my-burger-builder/src/components/Burger/Burger.js
+++ my-burger-builder/src/components/Burger/Burger.js
@@ -13,13 +13,20 @@ const burger = (props) => {
     //Array(props.ingredients[igKey] creates a new Array like new Array(3)
     //means the size of array is 3
     //here we creates array having size that equals the amount of given ingredient (igKey)
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map((igKey) => {
             return [...Array(props.ingredients[igKey])] 
                     .map((_, i) => {
                         return <BurgerIngredient key={igKey + i} type={igKey} />;
-                    });
-        });
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p className={classes.comment}>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
