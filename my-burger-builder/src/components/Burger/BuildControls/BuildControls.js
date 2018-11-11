842133234d0ffab66ba5d915c27247d35cb49838
moytri <moytritopa@gmail.com>
2018-11-11 10:34:41 -0800

    add Checkout option enable-disable

--- my-burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ my-burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -18,7 +18,9 @@ const buildControls = (props) => (
             added={() => props.ingredientAdded(control.type)}
             removed={() => props.ingredientRemoved(control.type)}
             disabled={props.disabled[control.type]}/>
-        ))}       
+        ))}   
+        <button className={classes.OrderButton}
+        disabled={!props.purchasable}>ORDER NOW</button>    
     </div>
 );
 
