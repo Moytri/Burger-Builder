2ae2bf37b407ba73ed050e544f77cbb069071658
moytri <moytritopa@gmail.com>
2018-11-10 17:54:22 -0800

    add const of ingredients type

--- my-burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ my-burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
 
-const buildControls = (props) => {
-    <div>
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'},
+];
 
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map((control) => (
+            <BuildControl key={control.label} 
+            label={control.label} 
+            added={() => props.ingredientAdded(control.type)}
+            removed={() => props.ingredientRemoved(control.type)}
+            disabled={props.disabled[control.type]}/>
+        ))}       
     </div>
-};
+);
 
 export default buildControls;
