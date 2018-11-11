648744bf370fbba8114763c4679ac53ba6049e73
moytri <moytritopa@gmail.com>
2018-11-11 11:11:31 -0800

    add selected ingredients to modal to show

--- my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -91,7 +93,10 @@ class BurgerBuilder extends Component {
         //graphical representation of the burger
         //The area where I need to add or remove Ingredients
         return (
-            <Aux>             
+            <Aux>  
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>        
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls ingredientAdded={this.addIngredientHandler}
                                ingredientRemoved={this.removeIngredientHandler}
