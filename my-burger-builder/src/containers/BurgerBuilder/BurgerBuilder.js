bc8ede3acfdc779c500156ba7b041adabbb7de58
moytri <moytritopa@gmail.com>
2018-11-13 14:24:55 -0800

    add two functions to continue and cancle order and pass as OrderSummary attribute

--- my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,6 +87,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false});
     }
 
+    purchaseContinueHandler = () => {
+        alert('continue');
+    }
+
     render() {
 
         const disabledInfo = {
@@ -104,7 +108,11 @@ class BurgerBuilder extends Component {
         return (
             <Aux>  
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCancled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
                 </Modal>        
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls ingredientAdded={this.addIngredientHandler}
