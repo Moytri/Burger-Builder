e2e15275290028092b57a639135524087f9c1a57
moytri <moytritopa@gmail.com>
2018-11-13 14:32:11 -0800

    add total price in summary modal

--- my-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ my-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price:  {props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button buttonType="Danger" clicked={props.purchaseCancled}>CANCEL</Button>
             <Button buttonType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
