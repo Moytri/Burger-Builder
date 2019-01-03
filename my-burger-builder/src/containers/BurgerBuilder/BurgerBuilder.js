680cd9279a9161de5fa1ed6025f6920e1f143f18
moytri <moytritopa@gmail.com>
2019-01-02 20:19:48 -0800

    connect to checkout

--- my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -99,30 +99,32 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('continue');
-        this.setState({loading:true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Moytri',
-                address: {
-                    street: 'Dow Ave',
-                    zipCode: 'V5',
-                    country: 'Canada'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        }
+        // this.setState({loading:true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Moytri',
+        //         address: {
+        //             street: 'Dow Ave',
+        //             zipCode: 'V5',
+        //             country: 'Canada'
+        //         },
+        //         email: 'test@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
 
-        axios.post('/orders.json', order)
-                .then(response => {
-                    this.setState({loading:false, purchasing:false});
-                } )
-                .catch(error => {
-                    //If there is an error, we stopped loading
-                    this.setState({loading:false, purchasing:false});
-                });
+        // axios.post('/orders.json', order)
+        //         .then(response => {
+        //             this.setState({loading:false, purchasing:false});
+        //         } )
+        //         .catch(error => {
+        //             //If there is an error, we stopped loading
+        //             this.setState({loading:false, purchasing:false});
+        //         });
+        this.props.history.push('/checkout');
+        
     }
 
     render() {
