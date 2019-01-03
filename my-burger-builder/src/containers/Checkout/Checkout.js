3bc46a6c81cffa4ecbe27f31e8581677a4648950
moytri <moytritopa@gmail.com>
2019-01-03 10:07:10 -0800

    add contact data router

--- my-burger-builder/src/containers/Checkout/Checkout.js
+++ my-burger-builder/src/containers/Checkout/Checkout.js
@@ -1,19 +1,44 @@
 import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import {Route} from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 1,
-            meat: 1
-        }
+        ingredients: ''
     }
+
+    componentDidMount () {
+        this.parseQueryParams();
+    }
+
+    parseQueryParams () {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients: ingredients});
+    }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render() {
+        console.log(this.props);
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients}/>
+                <CheckoutSummary 
+                    ingredients={this.state.ingredients}
+                    onCheckoutCancelled={this.checkoutCancelledHandler}
+                    onCheckoutContinued={this.checkoutContinuedHandler}/>
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
             </div>
         );
     }
