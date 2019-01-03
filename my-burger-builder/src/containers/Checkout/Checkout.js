6727ee6ec0fe89a0a5fc6887ef352b4a80d51744
moytri <moytritopa@gmail.com>
2019-01-03 11:13:42 -0800

    pass ingredients, totalPrice and props in ContactData

--- my-burger-builder/src/containers/Checkout/Checkout.js
+++ my-burger-builder/src/containers/Checkout/Checkout.js
@@ -5,21 +5,28 @@ import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: ''
+        ingredients: null,
+        price: 0
     }
 
-    componentDidMount () {
+    componentWillMount () {
         this.parseQueryParams();
     }
 
     parseQueryParams () {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
 
         for (let param of query.entries()) {
-            ingredients[param[0]] = +param[1];
+            if(param[0] === 'price') {
+                price = param[1];
+            }
+            else {
+                ingredients[param[0]] = +param[1];
+            }            
         }
-        this.setState({ingredients: ingredients});
+        this.setState({ingredients: ingredients, totalPrice: price});
     }
 
     checkoutCancelledHandler = () => {
@@ -38,7 +45,11 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     onCheckoutCancelled={this.checkoutCancelledHandler}
                     onCheckoutContinued={this.checkoutContinuedHandler}/>
-                <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
+                <Route path={this.props.match.path + '/contact-data'} 
+                    render={(props) => (<ContactData 
+                                    ingredients={this.state.ingredients}
+                                    price={this.state.totalPrice}
+                                    {...props}/>)}/>
             </div>
         );
     }
