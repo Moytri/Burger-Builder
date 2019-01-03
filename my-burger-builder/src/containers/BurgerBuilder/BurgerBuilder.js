d8384ceb9cded72a96442f698bc9e1fd217d27ab
moytri <moytritopa@gmail.com>
2019-01-03 11:14:36 -0800

    delete insert(fireBase) code

--- my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -98,36 +98,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        //alert('continue');
-        // this.setState({loading:true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Moytri',
-        //         address: {
-        //             street: 'Dow Ave',
-        //             zipCode: 'V5',
-        //             country: 'Canada'
-        //         },
-        //         email: 'test@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-
-        // axios.post('/orders.json', order)
-        //         .then(response => {
-        //             this.setState({loading:false, purchasing:false});
-        //         } )
-        //         .catch(error => {
-        //             //If there is an error, we stopped loading
-        //             this.setState({loading:false, purchasing:false});
-        //         });
         const queryParams = [];
         for(let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
         }
 
+        queryParams.push('price=' +this.state.totalPrice);
         const queryString = queryParams.join('&');
 
         this.props.history.push({
