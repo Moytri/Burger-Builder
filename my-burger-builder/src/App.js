850fd495fd2cd0fbdc8c201cfc2a1f52a596e558
moytri <moytritopa@gmail.com>
2019-01-03 11:35:34 -0800

    add orders route

--- my-burger-builder/src/App.js
+++ my-burger-builder/src/App.js
@@ -3,6 +3,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Layout> 
           <Switch>
               <Route path="/" exact component={BurgerBuilder}/>
+              <Route path="/orders" component={Orders}/>
               <Route path="/checkout" component={Checkout}/>
           </Switch>
         </Layout>
