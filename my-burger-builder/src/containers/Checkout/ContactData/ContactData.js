891cd4e65243957d94b1c1bcce74adfcfe719069
moytri <moytritopa@gmail.com>
2019-01-25 12:46:08 -0800

    add Input

--- my-burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ my-burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -48,10 +49,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
-                <input className={classes.Input} type="text" name="email" placeholder="Your Email"/>
-                <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-                <input className={classes.Input} type="text" name="postalCode" placeholder="PostalCode"/>
+                <Input inputtype="input" type="text" name="name" placeholder="Your Name"/>
+                <Input inputtype="input" type="text" name="email" placeholder="Your Email"/>
+                <Input inputtype="input" type="text" name="street" placeholder="Street"/>
+                <Input inputtype="input" type="text" name="postalCode" placeholder="PostalCode"/>
                 <Button buttonType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
