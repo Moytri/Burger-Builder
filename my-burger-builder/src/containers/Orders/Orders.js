3ef7134e5e7d281837b62ff802e335df11656e1b
moytri <moytritopa@gmail.com>
2019-01-25 12:42:30 -0800

    n/a

--- my-burger-builder/src/containers/Orders/Orders.js
+++ my-burger-builder/src/containers/Orders/Orders.js
@@ -1,15 +1,51 @@
 import React, {Component} from 'react';
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-import Order from '../../components/Order/Order'
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount () {
+        axios.get("/orders.json")
+        .then(response => {
+            const fethedOrders = [];
+            for(let key in response.data) {
+                fethedOrders.push({
+                    ...response.data[key],
+                    id: key
+                });                                 
+            }
+            this.setState({
+                loading: false,
+                orders: fethedOrders
+            });
+        })
+        .catch(err => {
+            this.setState({
+                loading: false 
+            })
+        });
+    }
+
     render () {
         return (
             <div>
-                <Order/>
-                <Order/>
+                {this.state.orders.map(
+                    order => (
+                        <Order 
+                            key={order.id}
+                            ingredients={order.ingredients}
+                            price={order.price}/>
+                    )
+                )}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders,axios);
