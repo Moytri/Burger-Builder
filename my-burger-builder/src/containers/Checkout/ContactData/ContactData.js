74b1567a42d3b0c18a3734cb7a467b5abceef6ec
moytri <moytritopa@gmail.com>
2019-01-25 13:46:47 -0800

    include dynamic input

--- my-burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ my-burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -7,11 +7,57 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest'},
+                        { value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            },
         },
         loading: false
     }
@@ -21,17 +67,7 @@ class ContactData extends Component {
         this.setState({loading:true});
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer: {
-                name: 'Moytri',
-                address: {
-                    street: 'Dow Ave',
-                    zipCode: 'V5',
-                    country: 'Canada'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price
         }
 
         axios.post('/orders.json', order)
@@ -47,18 +83,32 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementsArray = [];
+
+        for(let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name"/>
-                <Input inputtype="input" type="text" name="email" placeholder="Your Email"/>
-                <Input inputtype="input" type="text" name="street" placeholder="Street"/>
-                <Input inputtype="input" type="text" name="postalCode" placeholder="PostalCode"/>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                           key={formElement.id}
+                           elementType={formElement.config.elementType}
+                           elementConfig={formElement.config.elementConfig} 
+                           value={formElement.config.value}/>
+                ))}
                 <Button buttonType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
+
         if(this.state.loading) {
             form = <Spinner/>;
         }
+
         return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
