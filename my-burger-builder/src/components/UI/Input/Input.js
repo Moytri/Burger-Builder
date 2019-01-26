37bcef0e1e96b43699513ba73190dcaa22223de3
moytri <moytritopa@gmail.com>
2019-01-26 11:06:39 -0800

    add onchage Handler for user input

--- my-burger-builder/src/components/UI/Input/Input.js
+++ my-burger-builder/src/components/UI/Input/Input.js
@@ -9,12 +9,30 @@ const input = (props) => {
         case('input'): 
             inputElement = <input className={classes.InputElement}
                                 {...props.elementConfig} 
-                                value={props.value}/>;
+                                value={props.value}
+                                onChange={props.changed}/>;
             break;
         case('textarea'): 
             inputElement = <textarea className={classes.InputElement}
                                     {...props.elementConfig}
-                                    value={props.value}/>;
+                                    value={props.value}
+                                    onChange={props.changed}/>;
+            break;
+        case('select'): 
+            inputElement = (<select className={classes.InputElement}
+                                {...props.elementConfig}
+                                value={props.value}
+                                onChange={props.changed}>
+                                {props.elementConfig.options.map(
+                                    option => (
+                                        <option 
+                                            key={option.value}
+                                            value={option.value}>
+                                            {option.displayValue}
+                                        </option>
+                                    )
+                                )}
+                            </select>);
             break;
         default:
             inputElement = <input className={classes.InputElement} 
